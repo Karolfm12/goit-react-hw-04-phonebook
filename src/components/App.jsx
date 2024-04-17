c09bbae39b98a31239ced9690575f5a796e0b573
Karol Kotański <karolfm12@onet.eu>
2024-04-17 14:01:04 +0200

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,52 @@
 import { useState } from 'react';
 
 export const App = () => {
-  const [name, setName] = useState('');
+  const [userData, setUserData] = useState({ userName: '', phoneNumber: '' });
+
+  // const [contacts]
+
+  const handleOnChange = (e, name) => {
+    // const { name, value } = e.target;
+    setUserData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSumbit = e => {
+    e.preventDefault();
+    console.log(e);
+  };
 
   return (
     <>
       <h1>Phonebook</h1>
-      <div>
+      <form onSubmit={handleSumbit}>
         <h3>Name:</h3>
         <input
           type="text"
-          name="name"
+          name="userName"
+          value={userData.userName}
+          onChange={handleOnChange}
           // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
         <h3>Number:</h3>
         <input
-          type="text"
-          name="phone"
+          type="number"
+          name="phoneNumber"
+          value={userData.phoneNumber}
+          onChange={handleOnChange}
           // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          // required
+          required
         />
         <br />
-        <button onClick={() => setName()}>Add Contanct</button>
-      </div>
+        <button type="submit">Add Contanct</button>
+      </form>
+      <div>{userData.userName}</div>
+      <div>{userData.phoneNumber}</div>
     </>
   );
 };
